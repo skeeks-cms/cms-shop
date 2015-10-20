bdbfda7a9ab2d95afd8ecb8cbd8865f05c8d1ef4
Неля <nelya@rusoft.ru>
2015-10-20 17:27:33 +0300

    92195 Реализовать управление складами

--- models/ShopStore.php
+++ models/ShopStore.php
@@ -2,6 +2,7 @@
 
 namespace skeeks\cms\shop\models;
 
+use skeeks\cms\components\Cms;
 use skeeks\cms\kladr\models\KladrLocation;
 use skeeks\cms\models\CmsSite;
 use skeeks\cms\models\StorageFile;
@@ -57,7 +58,9 @@ class ShopStore extends \skeeks\cms\models\Core
             [['description'], 'string'],
             [['name', 'address', 'phone', 'schedule', 'xml_id', 'email'], 'string', 'max' => 255],
             [['active', 'issuing_center', 'shipping_center'], 'string', 'max' => 1],
-            [['gps_n', 'gps_s', 'site_code'], 'string', 'max' => 15]
+            [['gps_n', 'gps_s', 'site_code'], 'string', 'max' => 15],
+            [['priority',], 'default', 'value' =>  1],
+            [['active', 'issuing_center', 'shipping_center'], 'default', 'value' =>  Cms::BOOL_Y],
         ];
     }
 
@@ -67,27 +70,27 @@ class ShopStore extends \skeeks\cms\models\Core
     public function attributeLabels()
     {
         return [
-            'id' => Yii::t('app', 'ID'),
-            'created_by' => Yii::t('app', 'Created By'),
-            'updated_by' => Yii::t('app', 'Updated By'),
-            'created_at' => Yii::t('app', 'Created At'),
-            'updated_at' => Yii::t('app', 'Updated At'),
-            'name' => Yii::t('app', 'Name'),
-            'active' => Yii::t('app', 'Active'),
-            'address' => Yii::t('app', 'Address'),
-            'description' => Yii::t('app', 'Description'),
-            'gps_n' => Yii::t('app', 'Gps N'),
-            'gps_s' => Yii::t('app', 'Gps S'),
-            'image_id' => Yii::t('app', 'Image ID'),
-            'location_id' => Yii::t('app', 'Location ID'),
-            'phone' => Yii::t('app', 'Phone'),
-            'schedule' => Yii::t('app', 'Schedule'),
-            'xml_id' => Yii::t('app', 'Xml ID'),
-            'priority' => Yii::t('app', 'Priority'),
-            'email' => Yii::t('app', 'Email'),
-            'issuing_center' => Yii::t('app', 'Issuing Center'),
-            'shipping_center' => Yii::t('app', 'Shipping Center'),
-            'site_code' => Yii::t('app', 'Site Code'),
+            'id'                => \skeeks\cms\shop\Module::t('app', 'ID'),
+            'created_by'        => \skeeks\cms\shop\Module::t('app', 'Created By'),
+            'updated_by'        => \skeeks\cms\shop\Module::t('app', 'Updated By'),
+            'created_at'        => \skeeks\cms\shop\Module::t('app', 'Created At'),
+            'updated_at'        => \skeeks\cms\shop\Module::t('app', 'Updated At'),
+            'name'              => \skeeks\cms\shop\Module::t('app', 'Name'),
+            'active'            => \skeeks\cms\shop\Module::t('app', 'Active'),
+            'address'           => \skeeks\cms\shop\Module::t('app', 'Address'),
+            'description'       => \skeeks\cms\shop\Module::t('app', 'Description'),
+            'gps_n'             => \skeeks\cms\shop\Module::t('app', 'Gps N'),
+            'gps_s'             => \skeeks\cms\shop\Module::t('app', 'Gps S'),
+            'image_id'          => \skeeks\cms\shop\Module::t('app', 'Image ID'),
+            'location_id'       => \skeeks\cms\shop\Module::t('app', 'Location ID'),
+            'phone'             => \skeeks\cms\shop\Module::t('app', 'Phone'),
+            'schedule'          => \skeeks\cms\shop\Module::t('app', 'Schedule'),
+            'xml_id'            => \skeeks\cms\shop\Module::t('app', 'Xml ID'),
+            'priority'          => \skeeks\cms\shop\Module::t('app', 'Priority'),
+            'email'             => \skeeks\cms\shop\Module::t('app', 'Email'),
+            'issuing_center'    => \skeeks\cms\shop\Module::t('app', 'Issuing Center'),
+            'shipping_center'   => \skeeks\cms\shop\Module::t('app', 'Shipping Center'),
+            'site_code'         => \skeeks\cms\shop\Module::t('app', 'Site'),
         ];
     }
 
