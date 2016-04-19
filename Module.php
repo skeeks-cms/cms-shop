f07c5939a962b0abec8454b08adc655c29681852
Semenov <semenov@skeeks.com>
2016-04-19 18:43:43 +0300

    Removed skeeks/cms/base/Module

--- Module.php
+++ Module.php
@@ -14,37 +14,8 @@ class Module extends \yii\web\Module
 {
     public $controllerNamespace = 'skeeks\cms\shop\controllers';
 
-    static public $isRegisteredTranslations = false;
-
-    public function init()
-    {
-        parent::init();
-        self::registerTranslations();
-    }
-
-    static public function registerTranslations()
-    {
-        if (self::$isRegisteredTranslations === false)
-        {
-            if (!isset(\Yii::$app->i18n->translations['skeeks/shop/app']))
-            {
-                \Yii::$app->i18n->translations['skeeks/shop/app'] = [
-                    'class'             => 'yii\i18n\PhpMessageSource',
-                    'sourceLanguage'    => 'en-US',
-                    'basePath'          => '@skeeks/cms/shop/messages',
-                    'fileMap' => [
-                        'skeeks/shop/app' => 'app.php',
-                    ],
-                ];
-            }
-
-            self::$isRegisteredTranslations = true;
-        }
-    }
-
     public static function t($category, $message, $params = [], $language = null)
     {
-        self::registerTranslations();
         return \Yii::t('skeeks/shop/' . $category, $message, $params, $language);
     }
 }
