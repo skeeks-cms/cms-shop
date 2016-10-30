0c61bc1200a34d55a9cfd05d0df8f63ba09b07ce
Саша <semenov@skeeks.com>
2016-10-30 18:01:22 +0300

    Quantity column

--- views/admin-cms-content-element/index.php
+++ views/admin-cms-content-element/index.php
@@ -12,6 +12,18 @@
 
 $dataProvider->setSort(['defaultOrder' => ['published_at' => SORT_DESC]]);
 
+$sortAttr = $dataProvider->getSort()->attributes;
+$dataProvider->query->joinWith('shopProduct as sp');
+
+$dataProvider->getSort()->attributes = \yii\helpers\ArrayHelper::merge($sortAttr, [
+    'quantity' => [
+        'asc' => ['sp.quantity' => SORT_ASC],
+        'desc' => ['sp.quantity' => SORT_DESC],
+        'label' => \Yii::t('skeeks/shop/app', 'Available quantity'),
+        'default' => SORT_ASC
+    ]
+]);
+
 $cmsContent = null;
 if ($content_id = \Yii::$app->request->get('content_id'))
 {
@@ -23,6 +35,19 @@ if ($content_id = \Yii::$app->request->get('content_id'))
     $searchModel->content_id = $content_id;
 }
 $columns = \skeeks\cms\shop\controllers\AdminCmsContentElementController::getColumns($cmsContent, $dataProvider);
+
+$columns = \yii\helpers\ArrayHelper::merge($columns, [
+    [
+        'label' => \Yii::t('skeeks/shop/app', 'Available quantity'),
+        'class' => \yii\grid\DataColumn::class,
+        'visible' => false,
+        'attribute' => 'quantity',
+        'value' => function(\skeeks\cms\shop\models\ShopCmsContentElement $shopCmsContentElement)
+        {
+            return $shopCmsContentElement->shopProduct->quantity;
+        },
+    ]
+])
 ?>
 
 <? $pjax = \yii\widgets\Pjax::begin(); ?>
