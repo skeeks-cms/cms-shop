e9ff790bf43de4753d3dd54ae567255a2d85b179
Semenov <semenov@skeeks.com>
2022-08-23 14:48:51 +0300

    Оптимизации мобильной версии

--- src/queryFilter/ShopEavQueryFilterHandler.php
+++ src/queryFilter/ShopEavQueryFilterHandler.php
@@ -20,11 +20,31 @@ use yii\db\Expression;
  */
 class ShopEavQueryFilterHandler extends CmsEavQueryFilterHandler
 {
+
+    public function getElementIds()
+    {
+        if ($this->_elementIds === null) {
+            $this->_baseQuery->with = [];
+            $this->_baseQuery->select(['cms_content_element.id as id']);
+
+            $this->_elementIds = [];
+
+            if ($ids = $this->_baseQuery->column()) {
+                if ($ids) {
+                    $string_ids = implode(",", $ids);
+                    $this->elementIds = $ids;
+                }
+            }
+        }
+
+        return $this->_elementIds;
+    }
+
     /**
      * @param $query
      * @return $this
      */
-    public function initRPByQuery($query)
+    /*public function initRPByQuery($query)
     {
         if ($rps = $query->all()) {
             foreach ($rps as $rp) {
@@ -34,57 +54,22 @@ class ShopEavQueryFilterHandler extends CmsEavQueryFilterHandler
 
         /**
          * @var $query \yii\db\ActiveQuery
-         */
         $query = clone $this->baseQuery;
         $query->with = [];
         $query->select(['cms_content_element.id as id']);
 
         $this->elementIds = [];
         if ($ids = $query->column()) {
-
             if ($ids) {
                 $string_ids = implode(",", $ids);
-
-                //Добавить к этому предожения по товарам
-                /*$child_ids = CmsContentElement::find()->select(['id'])->where(new Expression("parent_content_element_id in ({$string_ids})"))->column();
-                if ($child_ids) {
-                    $ids = array_merge($ids, $child_ids);
-                }*/
-
-                //Если показывается сайт который собирает товары с других сайтов
-                /*$tmpIds = implode(",", $ids);
-                if (\Yii::$app->skeeks->site->shopSite->is_receiver) {
-                    //$mainIds = ShopProduct::find()->select(['main_pid'])->where(new Expression("id in ({$tmpIds})"))->andWhere(['is not', 'main_pid', null])->column();
-                    $mainIds = ShopCmsContentElement::find()
-                                ->joinWith("shopProduct as sp", true, "INNER JOIN")
-                                ->select(['main_cce_id'])->where(new Expression(ShopCmsContentElement::tableName() . ".id in ({$tmpIds})"))
-                                ->andWhere(['is not', 'main_cce_id', null])->column();
-
-                    if ($mainIds) {
-                        $ids = array_merge($ids, $mainIds);
-                    }
-                }*/
-                /*print_r($ids);die;*/
-
                 $this->elementIds = $ids;
-                /*$ids = implode(",", $ids);
-                $this->elementIds = CmsContentElement::find()
-                    ->andWhere(new Expression("id in ({$ids})"))
-                    ->select(['id'])
-                    ->column()
-                ;*/
-
             } else {
                 $this->elementIds = [];
             }
-
-
-            //$this->elementIds = $ids;
         }
 
-
         return $this;
-    }
+    }*/
 
 
     protected function _applyToQuery(ActiveQuery $activeQuery, $unionQuery) {
