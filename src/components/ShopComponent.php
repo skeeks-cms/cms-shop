e0310e96f7559b48465fc60d990a7e9bb675989b
Semenov <semenov@skeeks.com>
2024-02-21 20:26:10 +0300

    dev

--- src/components/ShopComponent.php
+++ src/components/ShopComponent.php
@@ -883,6 +883,8 @@ SQL
         $sql = file_get_contents($sqlFile);
         $sql = str_replace("{site_id}", $cmsSite->id, $sql);
 
+        /*print_r($sql);die;*/
+
         \Yii::$app->db->createCommand($sql)->execute();
     }
 
